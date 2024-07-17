8d12b56206b1cb051393d5111a8f04b3888a055b
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-07-18 00:11:12 +0200

    Migrations created

---
+++ Laravel/ticketing_system/database/migrations/2024_07_17_195033_create_ticket_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_types', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->string('type');
+            $table->decimal('price', 8, 2);
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_types');
+    }
+}
