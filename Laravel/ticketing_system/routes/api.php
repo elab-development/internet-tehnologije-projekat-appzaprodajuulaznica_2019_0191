6a852166f7b2b7335e11a816b3f5d5e49cd54786
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-07-19 20:27:11 +0200

    sanctum inatall

--- Laravel/ticketing_system/routes/api.php
+++ Laravel/ticketing_system/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketTypeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('events', EventController::class);
+    Route::apiResource('ticket-types', TicketTypeController::class);
+    Route::apiResource('orders', OrderController::class);
+});
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
