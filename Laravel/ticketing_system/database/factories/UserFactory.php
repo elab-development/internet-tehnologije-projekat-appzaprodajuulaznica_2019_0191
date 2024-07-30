1d80bd41bb77eb64996493a1c1c22dae744bd335
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-07-30 15:56:36 +0200

    User and Event factories

--- Laravel/ticketing_system/database/factories/UserFactory.php
+++ Laravel/ticketing_system/database/factories/UserFactory.php
@@ -7,18 +7,15 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = \App\Models\User::class;
+
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
