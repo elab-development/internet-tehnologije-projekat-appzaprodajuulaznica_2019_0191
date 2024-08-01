2749bca7fedf3afd9eea364fdfb257cf2824bc7d
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-08-01 20:01:59 +0200

    Login page

---
+++ React/ticketing-frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HeroWithLogin from '../components/herolog'
+
+export class Login extends Component {
+  render() {
+    return (
+      <div><HeroWithLogin/></div>
+    )
+  }
+}
+
+export default Login
