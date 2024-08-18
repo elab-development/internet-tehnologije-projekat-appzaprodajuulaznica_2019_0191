3a35bfb1bd2901c8c12d6bbf5890a858bf94c765
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-08-18 18:11:51 +0200

    navbar authorization update

--- React/ticketing-frontend/src/components/navBar.jsx
+++ React/ticketing-frontend/src/components/navBar.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import {Link} from 'react-router-dom';
+import { useAuth } from '../services/AuthContext';
 
 const Navbar = () => {
+  const { user, logout } = useAuth();
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My App
-        </Typography>
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">About</Button>
-        <Button color="inherit">Contact</Button>
+        {/* ... other buttons */}
+        {user ? (
+          <>
+            <Button color="inherit" onClick={logout}>Logout</Button>
+            {user.isAdmin && <Button color="inherit" component={Link} to="/admin">Admin</Button>}
+          </>
+        ) : (
+          <>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/login">Login</Button>
+            <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
