cefef336341b0ae23f6a4b1f62070d7de8bb099a
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-08-18 18:10:41 +0200

    Log component creation

---
+++ React/ticketing-frontend/src/components/log.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Typography, Button, Box, Container, TextField } from '@mui/material';
+
+const Log = () => {
+  return (
+    <> 
+      <Container component="main" maxWidth="xs" sx={{ mt: 4 }}>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Log;
