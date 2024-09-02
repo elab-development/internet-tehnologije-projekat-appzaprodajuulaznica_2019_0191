130c9c60ff7ecef645710b081023e30111082137
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-09-03 00:08:56 +0200

    registerAdmin and user functions

--- Laravel/ticketing_system/app/Http/Controllers/AuthController.php
+++ Laravel/ticketing_system/app/Http/Controllers/AuthController.php
@@ -25,39 +25,63 @@ class AuthController extends Controller
         ]);
 
         Auth::login($user);
-        return ;
+        return;
     }
 
     public function login(Request $request)
-{
-    $request->validate([
-        'email' => 'required|string|email',
-        'password' => 'required|string',
-    ]);
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
 
-    if (!Auth::attempt($request->only('email', 'password'))) {
-        return response()->json(['message' => 'Invalid login credentials'], 401);
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid login credentials'], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'isAdmin' => $user->is_admin,
+            ]
+        ]);
     }
-
-    $user = $request->user();
-    $token = $user->createToken('auth_token')->plainTextToken;
-
-    return response()->json([
-        'access_token' => $token,
-        'token_type' => 'Bearer',
-        'user' => [
-            'id' => $user->id,
-            'name' => $user->name,
-            'email' => $user->email,
-            //'isAdmin' => $user->isAdmin,
-        ]
-    ]);
-}
 
     public function logout(Request $request)
     {
         $request->user()->tokens()->delete();
 
         return response()->json(['message' => 'Logged out successfully']);
+    }
+
+    public function registerAdmin(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'is_admin' => true,
+        ]);
+
+        return response()->json(['message' => 'Admin created successfully']);; // Redirect to admin dashboard after registration
+    }
+
+    public function user(Request $request)
+    {
+        $user = $request->user();
+        return response()->json($user);
     }
 }
