6ec4ce561463072879d8b4b4ae44419c69c72f45
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-09-02 13:06:49 +0200

    event controller functions update

--- Laravel/ticketing_system/app/Http/Controllers/EventController.php
+++ Laravel/ticketing_system/app/Http/Controllers/EventController.php
@@ -4,16 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\TicketType;
 
 class EventController extends Controller
 {
     public function index()
     {
+        $events = Event::paginate(8);
+        return response()->json($events);
+    }
+
+    public function adminIndex()
+    {
         $events = Event::all();
         return response()->json($events);
     }
 
-    public function store(Request $request)
+    public function addTicket(Request $request, $id)
+    {
+        $request->validate([
+            'type' => 'required|string|in:VIP,REGULAR,GENERAL',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:1',
+        ]);
+
+        $event = Event::findOrFail($id);
+        $ticketType = new TicketType($request->all());
+        $event->ticketTypes()->save($ticketType);
+
+        return response()->json($ticketType, 201);
+    }
+   public function store(Request $request)
     {
         $request->validate([
             'name' => 'required|string|max:255',
@@ -22,30 +43,36 @@ class EventController extends Controller
         ]);
 
         $event = Event::create($request->all());
+
         return response()->json($event, 201);
     }
 
     public function show($id)
     {
-        return Event::findOrFail($id);
+        $event = Event::with('ticketTypes')->findOrFail($id);
+        return response()->json($event);
     }
 
     public function update(Request $request, $id)
     {
+        $event = Event::findOrFail($id);
+
         $request->validate([
-            'name' => 'sometimes|required|string|max:255',
-            'description' => 'sometimes|required|string',
-            'event_date' => 'sometimes|required|date',
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'event_date' => 'required|date',
         ]);
 
-        $event = Event::findOrFail($id);
         $event->update($request->all());
+
         return response()->json($event, 200);
     }
 
     public function destroy($id)
     {
-        Event::destroy($id);
+        $event = Event::findOrFail($id);
+        $event->delete();
+
         return response()->json(null, 204);
     }
 }
