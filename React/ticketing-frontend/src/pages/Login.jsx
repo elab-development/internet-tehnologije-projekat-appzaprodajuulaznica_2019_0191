1fcd902d1795ea54fc8e51c616dcaaf58ae7930e
Veljko Cvetkovic <cvetkovicv72@gmail.com>
2024-09-02 13:09:13 +0200

    login page handle functions

--- React/ticketing-frontend/src/pages/Login.jsx
+++ React/ticketing-frontend/src/pages/Login.jsx
@@ -1,12 +1,79 @@
-import React, { Component } from 'react'
-import HeroWithLogin from '../components/herolog'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../services/AuthContext'
+import http from '../utils/http';
+import Log from '../components/log';
+import { Typography, Button, Box, Container, TextField } from '@mui/material';
 
-export class Login extends Component {
-  render() {
-    return (
-      <div><HeroWithLogin/></div>
-    )
-  }
-}
+const Login = () => {
+  const [logInData, setLogInData] = useState({
+    email :'',
+    password:'',
+  });
+  const navigate = useNavigate();
+  const { login } = useAuth();
 
-export default Login
+  const handleChange = (e) => {
+    setLogInData({ ...logInData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await http.post('api/login', logInData);
+      login(response.data.user, response.data.access_token);
+      navigate('/');
+    } catch (error) {
+      console.error('Login failed', error);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs" sx={{ mt: 4 }}>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+  );
+};
+export default Login;
